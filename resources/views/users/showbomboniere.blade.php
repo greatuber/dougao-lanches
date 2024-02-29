efd9e8588c81cd4d41a54889d3ccd239a30b7fa6
greatuber123 <greatuber123@gmail.com>
2024-02-29 18:16:44 -0300

    ajustando loyaut da tela de vendas dos combos

--- resources/views/users/showbomboniere.blade.php
+++ resources/views/users/showbomboniere.blade.php
@@ -14,8 +14,7 @@
 <link href="https://fonts.googleapis.com/css2?family=Chela+One&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <style>
-      .container {
-      }
+   
       .orange {
         border-radius: 8px;
         margin-right: 8px; 
@@ -32,6 +31,9 @@
             margin-top: 15px;
             padding-right: 10px;
           }
+          .color{
+      background-color: rgba(223, 91, 91, 0.726);
+    }
     </style>
     <title>CreateProduct</title>
 </head>
@@ -59,7 +61,7 @@
           <div class="bg-orange-500">
             @include('layouts.menu')
           </div>
-        <table class="w-full ">
+        <table class="w-full overflow-auto ">
           <thead>
             <tr>
                  <th></th>      
